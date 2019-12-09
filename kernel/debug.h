c6d9fe11c9cbb82d2244175a5132838ae4816b92
Fnu Harsh <hars@attu8.cs.washington.edu>
2019-12-08 19:05:58 -0800

    Switched off all logging.

--- kernel/debug.h
+++ kernel/debug.h
@@ -1,6 +1,6 @@
-#define ENABLE_TLB_SHOOTDOWN       1 // If 0, turns TLB shoot down invocations into a n
+#define ENABLE_TLB_SHOOTDOWN       0 // If 0, turns TLB shoot down invocations into a n
 
 #define LOG_ALWAYS_TLB_SHOOTDOWN   0 // Whether to log TLB shootdown progress by default
 
-#define LOG_ALWAYS_TIMER           1 // Whether to log all device interrupts
-#define LONG_TIMER_INTERVAL        1 // Make the time interrupt a lot longer
+#define LOG_ALWAYS_TIMER           0 // Whether to log all device interrupts
+#define LONG_TIMER_INTERVAL        0 // Make the time interrupt a lot longer
